cf75632a5ce2cb449ca7c7c8aca83ae45e289a47
Quan <quandm11112005@gmail.com>
2024-09-30 23:43:59 +0700

    test

--- src/main/java/org/example/demo/App.java
+++ src/main/java/org/example/demo/App.java
@@ -18,15 +18,18 @@ import static javafx.application.Application.launch;
 
 
 public class App extends Application {
+
   @Override
-  public void start(Stage primaryStage) throws Exception{
-    Parent root=FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/org/example/demo/FXML/Base.fxml")));
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(
+        Objects.requireNonNull(getClass().getResource("/org/example/demo/FXML/Base.fxml")));
     primaryStage.setTitle("Library Management");
     primaryStage.setResizable(false);
     primaryStage.setScene(new Scene(root));
     primaryStage.show();
   }
-  public static void main(String[]args){
+
+  public static void main(String[] args) {
     launch(args);
   }
 }
