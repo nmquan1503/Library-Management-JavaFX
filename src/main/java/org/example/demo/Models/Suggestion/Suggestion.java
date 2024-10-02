74d5c8a3e3813bd5fb2abb35dfcac4ec4c3dc56a
nmquan1503 <minhquan15032005@gmail.com>
2024-10-02 23:19:03 +0700

    complete suggestion class

--- src/main/java/org/example/demo/Models/Suggestion/Suggestion.java
+++ src/main/java/org/example/demo/Models/Suggestion/Suggestion.java
@@ -2,7 +2,47 @@ package org.example.demo.Models.Suggestion;
 
 import javafx.scene.image.Image;
 import org.example.demo.Models.BookShelf.Book;
+import org.example.demo.Models.Users.User;
 
 public class Suggestion {
 
+  private int id;
+  private Image icon;
+  private String content;
+
+  public Suggestion(Book book) {
+    id = book.getId();
+    icon = new Image(book.getImageLink());
+    content = book.getTitle();
+  }
+
+  public Suggestion(User user) {
+    id = user.getId();
+    icon = user.getImage();
+    content = user.getName();
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public Image getIcon() {
+    return icon;
+  }
+
+  public void setIcon(Image icon) {
+    this.icon = icon;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
 }
