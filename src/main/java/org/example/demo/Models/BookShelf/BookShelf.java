b4e98bd813fe028fc0f5b0fce95d8c95e33191c9
DXT0509 <thaodao05092005@gmail.com>
2024-10-03 21:43:54 +0700

    update=))

--- src/main/java/org/example/demo/Models/BookShelf/BookShelf.java
+++ src/main/java/org/example/demo/Models/BookShelf/BookShelf.java
@@ -8,19 +8,4 @@ import java.util.Date;
 import org.example.demo.Models.BookShelf.Book;
 public class BookShelf {
 
-
-    public static void main(String args[]) {
-        ArrayList<String> authors = new ArrayList<>();
-        ArrayList<String> cate = new ArrayList<>();
-        authors.add("THAODAO");
-        authors.add("XUÂNQUYEN");
-        authors.add("ĐÀO");
-        cate.add("trinh thám");
-        cate.add("tiểu thuyết");
-        cate.add("hài kịch");
-
-        Book dxt = new Book("Harry Potter 2", authors, "KIM ĐỒNG", 2024, "Story about a wizard", 203, cate, 100, 4.5, "thisisimagelink");
-        dxt.SaveInfo();
-
-    }
 }
