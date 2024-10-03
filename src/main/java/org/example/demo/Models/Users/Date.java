d3a1820c0b4af85a928d1c52c5782d0b5af26411
23020138 <23020138@vnu.edu.vn>
2024-10-03 20:59:24 +0700

    add function in Date

--- src/main/java/org/example/demo/Models/Users/Date.java
+++ src/main/java/org/example/demo/Models/Users/Date.java
@@ -1,8 +1,24 @@
 package org.example.demo.Models.Users;
 
+import java.util.Calendar;
+
 public class Date extends java.sql.Date {
 
   public Date(int year, int month, int day) {
     super(year - 1900, month - 1, day);
+  }
+
+  public Date add(int days) {
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(this);
+    calendar.add(Calendar.DAY_OF_MONTH, days);
+    return new Date(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1,
+        calendar.get(Calendar.DAY_OF_MONTH));
+  }
+
+  public static void main(String[] args) {
+    Date myDate = new Date(2024, 10, 3); // Create a date
+    Date newDate = myDate.add(31); // Add 5 days
+    System.out.println("New Date: " + newDate);
   }
 }
